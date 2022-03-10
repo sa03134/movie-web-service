b06e0c998e5bbe456e5437dcb3fb8c445eaba548
Jung Woo Sohn <jungwoosohn71@gmail.com>
2022-03-10 21:05:02 +0900

    feat : CRA useEffect function to control specific state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  const onClick = () => setValue((prev) => prev + 1);
+
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
+
+  useEffect(() => {
+    console.log("I run when keyword changes");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when counter changes");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run when keyword & counter changes");
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
       <Button text="Continue" />
     </div>
   );
