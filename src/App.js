6b7354790f84e7845e33ed2fda7f6ef59861f41f
Jung Woo Sohn <jungwoosohn71@gmail.com>
2022-03-12 23:53:20 +0900

    feat : fetch URL for coin price api

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-
-  const onChange = (event) => {
-    return setToDo(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers");
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins!</h1>
+      {loading ? <strong> Loading... </strong> : null}
     </div>
   );
 }
