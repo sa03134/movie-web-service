6fbb84c9499e3a4f94f9681a6037b2139dcf60cf
Jung Woo Sohn <jungwoosohn71@gmail.com>
2022-03-14 23:51:55 +0900

    fix : coin api data request

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers");
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=2000")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
   }, []);
   return (
     <div>
