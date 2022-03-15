4f18d5038b8941207375fe48ebdbe0d0bbf8461e
Jung Woo Sohn <jungwoosohn71@gmail.com>
2022-03-15 15:43:45 +0900

    feat: implement coin tracker using api

--- src/App.js
+++ src/App.js
@@ -4,17 +4,38 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=2000")
+    fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
         setLoading(false);
       });
   }, []);
+  const [inputUSD, setInputUSD] = useState(0);
+
+  const onChange = (event) => {
+    setInputUSD(event.target.value);
+  };
+
   return (
     <div>
-      <h1>The Coins!</h1>
-      {loading ? <strong> Loading... </strong> : null}
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong> Loading... </strong>
+      ) : (
+        <div>
+          <input type="number" onChange={onChange} placeholder="0 USD"></input>
+          <h1>get coins with {inputUSD} $ </h1>
+          <ul>
+            {coins.map((coin) => (
+              <li>
+                {inputUSD / coin.quotes.USD.price}
+                {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
