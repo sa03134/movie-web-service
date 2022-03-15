7bebbabd2bb5571b64938a2243b1767f7dc1f629
Jung Woo Sohn <jungwoosohn71@gmail.com>
2022-03-15 22:11:32 +0900

    feat : implement async-await movie api

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  const [inputUSD, setInputUSD] = useState(0);
-
-  const onChange = (event) => {
-    setInputUSD(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
 
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong> Loading... </strong>
+        <h1>Loading...</h1>
       ) : (
         <div>
-          <input type="number" onChange={onChange} placeholder="0 USD"></input>
-          <h1>get coins with {inputUSD} $ </h1>
-          <ul>
-            {coins.map((coin) => (
-              <li>
-                {inputUSD / coin.quotes.USD.price}
-                {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-              </li>
-            ))}
-          </ul>
+          {movies.map((movie) => (
+            <div key={movie.id}>{movie.title}</div>
+          ))}
         </div>
-      )}
+      )}{" "}
     </div>
   );
 }
